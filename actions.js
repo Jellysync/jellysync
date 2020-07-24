a8c153186ec84fe49aa4361af2cc20f92a89f683
Avi Prince <avisprince@gmail.com>
2020-07-23 23:26:40 -0400

    use micromodal

--- actions.js
+++ actions.js
@@ -1,6 +1,8 @@
-require('./actions.css');
+require('./microModal.css');
+
 const jsCookie = require('js-cookie');
-const { Modal } = require('antd');
+const MicroModal = require('micromodal').default;
+const $ = require('jquery');
 
 function showUpdateModal(snapshot, clearCache) {
   const { version, showModalOnForce, refreshTime, modalText } = snapshot;
@@ -9,32 +11,53 @@ function showUpdateModal(snapshot, clearCache) {
     return location.reload(clearCache);
   }
 
-  let secondsToGo = refreshTime;
+  $(document.body).append(`
+    <div class="modal micromodal-slide" id="jellysync-modal" aria-hidden="true">
+      <div class="modal__overlay" tabindex="-1">
+        <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
+          <header class="modal__header">
+            <h2 class="modal__title" id="modal-1-title">
+              Update to version ${version}
+            </h2>
+          </header>
+          <main class="modal__content" id="modal-1-content">
+            <p>${modalText}</p>
+            <p class="modal__countdown">Auto refreshing in ${refreshTime} seconds.</p>
+          </main>
+          <footer class="modal__footer">
+            <button class="modal__btn modal__btn-primary jellysync_update_button">Update</button>
+          </footer>
+        </div>
+      </div>
+    </div>
+  `);
 
-  const modal = Modal.info({
-    className: 'jellySyncUpdateModal',
-    title: `Update to version ${version}`,
-    content: `${modalText}\n\nAuto refreshing in ${secondsToGo} seconds.`,
-    keyboard: false,
-    okText: 'Update',
-    onOk() {
-      location.reload(clearCache);
+  $(document).keydown(function (event) {
+    if (event.keyCode === 27) {
+      event.stopImmediatePropagation();
     }
   });
+
+  MicroModal.init();
+  MicroModal.show('jellysync-modal');
+
+  let secondsToGo = refreshTime;
 
   const timer = setInterval(() => {
     secondsToGo -= 1;
 
-    modal.update({
-      content: `${modalText}\n\nAuto refreshing in ${secondsToGo} seconds.`
-    });
+    $('.modal__countdown').text(`Auto refreshing in ${secondsToGo} seconds.`);
   }, 1000);
 
   setTimeout(() => {
     clearInterval(timer);
-
     location.reload(clearCache);
   }, secondsToGo * 1000);
+
+  $('.jellysync_update_button').click(() => {
+    clearInterval(timer);
+    location.reload(clearCache);
+  });
 }
 
 function forceUpdate(snapshot) {
