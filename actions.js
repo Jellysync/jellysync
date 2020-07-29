03e6b3049e0c6ba8a7cb51d0c63ad3848a416053
Avi Prince <avisprince@gmail.com>
2020-07-29 01:09:35 -0400

    babel for the win

--- actions.js
+++ actions.js
@@ -1,31 +1,35 @@
-require('./microModal.css');
+import './microModal.css';
+import MicroModal from 'micromodal';
+import jsCookie from 'js-cookie';
+import $ from 'jquery';
 
-const jsCookie = require('js-cookie');
-const MicroModal = require('micromodal').default;
-const $ = require('jquery');
-
-function showUpdateModal(snapshot, clearCache) {
+export function showUpdateModal(snapshot, clearCache) {
   const { version, showModalOnForce, refreshTime, modalText } = snapshot;
+
+  // Don't do anything if a modal is already open
+  if ($('.jellysync_modal').length) {
+    return;
+  }
 
   if (!showModalOnForce) {
     return location.reload(clearCache);
   }
 
   $(document.body).append(`
-    <div class="jellysync_modal micromodal-slide" id="jellysync-modal" aria-hidden="true">
-      <div class="modal__overlay" tabindex="-1">
-        <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
-          <header class="modal__header">
-            <h2 class="modal__title" id="modal-1-title">
+    <div class="jellysync_modal" id="jellysync-modal" aria-hidden="true">
+      <div class="jellysync_modal__overlay" tabindex="-1">
+        <div class="jellysync_modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
+          <header class="jellysync_modal__header">
+            <h2 class="jellysync_modal__title" id="modal-1-title">
               Update to version ${version}
             </h2>
           </header>
-          <main class="modal__content" id="modal-1-content">
+          <main class="jellysync_modal__content" id="modal-1-content">
             <p>${modalText}</p>
-            <p class="modal__countdown">Auto refreshing in ${refreshTime} seconds.</p>
+            <p class="jellysync_modal__countdown">Auto refreshing in ${refreshTime} seconds.</p>
           </main>
-          <footer class="modal__footer">
-            <button class="modal__btn modal__btn-primary jellysync_update_button">Update</button>
+          <footer class="jellysync_modal__footer">
+            <button class="jellysync_modal__btn jellysync_modal__btn-primary jellysync_update_button">Update</button>
           </footer>
         </div>
       </div>
@@ -51,7 +55,7 @@ function showUpdateModal(snapshot, clearCache) {
   timer = setInterval(() => {
     secondsToGo -= 1;
 
-    $('.modal__countdown').text(`Auto refreshing in ${secondsToGo} seconds.`);
+    $('.jellysync_modal__countdown').text(`Auto refreshing in ${secondsToGo} seconds.`);
 
     if (secondsToGo <= 0) {
       MicroModal.close('jellysync-modal');
@@ -63,7 +67,7 @@ function showUpdateModal(snapshot, clearCache) {
   });
 }
 
-function forceUpdate(snapshot) {
+export function forceUpdate(snapshot) {
   if (snapshot.initialLoad) {
     location.reload(false);
     return;
@@ -72,7 +76,7 @@ function forceUpdate(snapshot) {
   showUpdateModal(snapshot, false);
 }
 
-function clearCache(snapshot) {
+export function clearCache(snapshot) {
   if (snapshot.initialLoad) {
     location.reload(true);
     return;
@@ -81,22 +85,14 @@ function clearCache(snapshot) {
   showUpdateModal(snapshot, true);
 }
 
-function clearLocalStorage() {
+export function clearLocalStorage() {
   localStorage.clear();
 }
 
-function clearSessionStorage() {
+export function clearSessionStorage() {
   sessionStorage.clear();
 }
 
-function clearCookies() {
+export function clearCookies() {
   Object.keys(jsCookie.get()).forEach(cookie => jsCookie.remove(cookie));
 }
-
-module.exports = {
-  forceUpdate,
-  clearCache,
-  clearCookies,
-  clearLocalStorage,
-  clearSessionStorage
-};
